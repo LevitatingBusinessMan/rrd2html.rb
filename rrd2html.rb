fb514c7ed2b815d809ab88a92f7be6e6bb4df77e
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2024-05-29 18:19:14 +0200

    support inversion

--- rrd2html.rb
+++ rrd2html.rb
@@ -3,15 +3,18 @@ require 'tomlrb'
 require 'base64'
 $conf = Tomlrb.load_file ARGV[0] || "/etc/rrd2html.toml", symbolize_keys: true
 
-$genline = ->(l, i) {"DEF:d#{i}=#{File.join($conf[:datadir], l[:file])}:#{l[:val] || $conf[:val] || 'value:AVERAGE'} \
-LINE:d#{i}#{l[:color] || $conf[:color] || '#FF0000'}:#{l[:name].inspect}"}
+$genline = ->(l, i) {"\
+DEF:d#{i}=#{File.join($conf[:datadir], l[:file])}:#{l[:val] || $conf[:val] || 'value:AVERAGE'} \
+#{"CDEF:d#{i}i=d#{i},-1,*" if l[:inv]} \
+LINE:d#{i}#{"i" if l[:inv]}#{l[:color] || $conf[:color] || '#FF0000'}:#{l[:name].inspect} \
+"}
 
 def render graph
     Base64.encode64`
         rrdtool graph - \
         --start=end-#{graph[:span] || $conf[:span] || "1h"} \
         --end=now \
-        #{graph[:title] ? "--title=#{graph[:title].inspect}" : ''} \
+        #{"--title=#{graph[:title].inspect}" if graph[:title]} \
         #{graph[:lines].each_with_index.map(&$genline).join(" ")} \
         --width=#{(graph[:dimensions] || $conf[:dimensions] || [300,100]).join ' --height='}} \
     `
